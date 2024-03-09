9108c0e700a5400354d4b75f1c0c3957c9bacec9
Audrey Clark <amazon.audrey@gmail.com>
2024-03-08 22:49:02 -0600

    Finish functionality

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-// your code here
+
+document.getElementById('submit').addEventListener('click', function () {
+  const name = document.getElementById('name').value;
+  const text = document.getElementById('message').value;
+
+  const postsDiv = document.querySelector('.posts');
+
+  const newPost = document.createElement('div');
+
+  const newPostText = document.createElement('p');
+  const newPostTextNode = document.createTextNode(text);
+  newPostText.append(newPostTextNode);
+
+  const newPostName = document.createElement('p');
+  const newPostNameNode = document.createTextNode('Posted By: ' + name);
+  newPostName.appendChild(newPostNameNode);
+
+  const newPostHr = document.createElement('hr');
+
+  newPost.append(newPostText);
+  newPost.append(newPostName);
+  newPost.append(newPostHr);
+
+  postsDiv.append(newPost);
+
+  console.log(name, text);
+});
+
